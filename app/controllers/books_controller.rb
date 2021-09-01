9295d0ee09d9c94d4a4adf5ceb337cad45ac87ab
makaroniletus1783 <daoyeyoushu@gmail.com>
2021-09-01 04:02:23 +0000

    [Start]アプリケーションを完成させよう２　10章

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = User.find(current_user.id)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "You have created book successfully"
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
+
+  end
+
+  def index
+    @books = Book.all
+    @user = User.find(current_user.id)
+    @book = Book.new
+  end
+
+  def show
+    @books = Book.find(params[:id])
+    @book = Book.new
+    @user = User.find(@books.user.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully"
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'aba8358deb47c6f6f0f8d6e49ce1fa63a0d1616bfa24e02a30246464152ba6667f16a79089c4fecba36b8fd370cec9f5964e85e27fec67fe60682cd8e685c601'
